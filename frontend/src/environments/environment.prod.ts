3ffbacd24c458b694d5f1268860a698f9721be9a
João Sá <joaofilipesa2020@gmail.com>
2026-02-02 13:50:42 +0000

    deploy(fix): env variables try

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+// src/environments/environment.prod.ts
+export const environment = {
+  production: true,
+  backendApiUrl: '__BACKEND_URL__' + '/api', // Your backend URL
+  socketUrl: '__BACKEND_URL__',
+};
---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,6 @@
+// src/environments/environment.ts
+export const environment = {
+  production: false,
+  backendApiUrl: 'http://localhost:4000/api', // Your backend URL
+  socketUrl: 'http://localhost:4000',
+};
