60c3cc1b7185a31edf3a8efb9891a4b3d4d724fc
devesa01 <joaodevesa2019@gmail..com>
2026-03-17 19:47:48 +0000

    refactor: deploy with https and nginx test

---
+++ scripts/init-ssl.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+DOMAIN="devas.pt"
+EMAIL="joaodevesa2019@gmail.com"
+APP_DIR="/home/devas/www/cards-without-humanity"
+
+cd "$APP_DIR"
+
+echo ">>> A subir Nginx em modo HTTP (para o desafio ACME)..."
+docker compose up -d nginx
+
+echo ">>> A obter certificado inicial..."
+docker compose run --rm certbot certonly \
+  --webroot \
+  --webroot-path=/var/www/certbot \
+  --email "$EMAIL" \
+  --agree-tos \
+  --no-eff-email \
+  -d "$DOMAIN" \
+  -d "www.$DOMAIN"
+
+echo ">>> A reiniciar Nginx com SSL ativo..."
+docker compose restart nginx
+
+echo ">>> SSL configurado com sucesso!"
