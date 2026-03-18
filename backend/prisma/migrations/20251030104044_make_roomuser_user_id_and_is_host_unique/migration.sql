89aa8ea386a681adbbb8fb312cbe19547449a890
João Sá <joaofilipesa2020@gmail.com>
2026-03-18 23:04:29 +0000

    fix: migrations updated because of linux errors

--- backend/prisma/migrations/20251030104044_make_roomuser_user_id_and_is_host_unique/migration.sql
+++ backend/prisma/migrations/20251030104044_make_roomuser_user_id_and_is_host_unique/migration.sql
@@ -1,8 +1,8 @@
 /*
   Warnings:
 
-  - A unique constraint covering the columns `[userId,isHost]` on the table `RoomUser` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[userId,isHost]` on the table `roomuser` will be added. If there are existing duplicate values, this will fail. 
 
 */
 -- CreateIndex
-CREATE UNIQUE INDEX `RoomUser_userId_isHost_key` ON `RoomUser`(`userId`, `isHost`);
+CREATE UNIQUE INDEX `RoomUser_userId_isHost_key` ON `roomuser`(`userId`, `isHost`);
