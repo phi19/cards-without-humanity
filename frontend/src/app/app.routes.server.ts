b8403a3a1c65b5f8136776eef9bd7e63a5be58ce
devesa01 <joaodevesa2019@gmail..com>
2026-03-18 23:32:02 +0000

    feat: add missing routes to production

--- frontend/src/app/app.routes.server.ts
+++ frontend/src/app/app.routes.server.ts
@@ -1,8 +1,47 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  { path: 'home', renderMode: RenderMode.Prerender },
+
+  // Decks Routes
+  {
+    path: 'decks/list-decks',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'decks/create-deck',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'login',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'rooms-list',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'room/:roomId',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'rooms-list',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'home',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'login',
+    renderMode: RenderMode.Server,
+  },
   {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: '',
+    renderMode: RenderMode.Prerender,
+  },
 ];
